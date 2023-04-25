ec33b0549e9fa43f6080f4d056182f7f13f83140
Mario <mcalvarr@ucm.es>
2023-04-25 10:47:48 +0200

    Jacobi

--- Jacobi_CalvarroMarinesMario.m
+++ Jacobi_CalvarroMarinesMario.m
@@ -1,6 +1,9 @@
 % € será el error aceptable
+
 % No convergencia viendo que ||x*-x|| < €||x|| no tiene sentido porque desconocemos x
+
 % Una forma mejor es ver que la diferencia de x_i e x_i-1 es suficientemente pequeño. No es buen criterio porque es posbile que la convergencia sea muy lenta.
+
 % Otra forma es sustituir x^k en Ax = b y ver el error respecto de b: -r_k = ||Ax_k - b|| < €||b||
 % Para calcular r_k no podemos hacerlo directamente porque sería muy costoso. Por tanto, lo que hacemos es:
 % D_x_{k+1} = (E + F)x_k + b = Dx_k (- Ax_k + b). El paréntesis es r_k 
@@ -13,9 +16,24 @@
 % Salida PA = LU:
 %   1) x: Solución aproximada del sistema. Vector n
 %   2) E: Escalar con la precisión del problema
-function [x, E] = Jacobi_CalvarroMarinesMario(A, b)
-    k = 100000; %Máximo nº de iteraciones TODO: Cambiar dependiente al tamaño de la matriz
-    E = 0.001; %Precisión del resultado
+%   3) conv: Booleano que indica si el método converge
+function [x, E, conv] = Jacobi_CalvarroMarinesMario(A, b)
     [n, ~] = size(A);
-    
+    iteraciones = n^2; %Máximo nº de iteraciones 
+    E = 0.001; %Precisión del resultado
+    conv = 0;
+
+    norma_b = norm(b);
+    r = zeros(n, 1);
+    d = zeros(n, 1);
+    x = zeros(n, 1);
+    for k = 1:iteraciones
+        r = b - A*x;
+        if norma_b * E > norm(r)
+            conv = 1;
+            break;
+        end
+        d = r ./ diag(A);
+        x = x + d;
+    end
 end 
