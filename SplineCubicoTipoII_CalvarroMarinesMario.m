d31eb08c5299f76ba81d65e1b686ca2cea0137e5
mario <mariocm2002@hotmail.com>
2023-04-29 14:20:49 +0200

    Entrega 7

--- SplineCubicoTipoII_CalvarroMarinesMario.m
+++ SplineCubicoTipoII_CalvarroMarinesMario.m
@@ -1,8 +1,8 @@
-% Entrada:
+% Entrada (vectores horizontales):
 %   1) x: Vector n+1 de puntos a interpolar
 %   2) y: Vector de las imágenes de x
-%   3) y0: Valor de la derivada en el extremo inicial
-%   4) y1: Valor de la derivada en el extremo final
+%   3) y0p: Valor de la derivada en el extremo inicial
+%   4) ynp: Valor de la derivada en el extremo final
 %   5) X: Vector n donde queremos conocer la interpolación
 % Salida:
 %   1) Y: Imágenes del vector X por la interpolación
